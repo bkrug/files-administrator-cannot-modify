94f9cedb73f355d60360aff0180a5fafff69f97f
bkrug <benjaminkrug@yahoo.com>
2025-02-19 21:18:16 -0500

    Add project files.

---
+++ ListFilePermissions/Program.cs
@@ -0,0 +1,82 @@
+﻿//
+// Prints a list of all files that the user does not have write access to.
+//
+
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+string filePath = @"I:\OneDrive";
+Console.WriteLine($"Searching within {filePath}.");
+var sum = PrintFilesWithoutWriteAccess(new DirectoryInfo(filePath));
+Console.WriteLine($"Done. {sum} files.");
+
+static int PrintFilesWithoutWriteAccess(DirectoryInfo parentDir)
+{
+    var files = FilesWithoutWriteAccess(parentDir).ToList();
+    var dirs = DirectoriesWithoutWriteAccess(parentDir).ToList();
+    foreach (var file in files)
+        Console.WriteLine($"{file.FullName}");
+    foreach (var dir in dirs)
+        Console.WriteLine($"{dir.FullName}");
+
+    return files.Count
+        + dirs.Count
+        + parentDir.GetDirectories().Sum(subDir => PrintFilesWithoutWriteAccess(subDir));
+}
+
+static IEnumerable<FileInfo> FilesWithoutWriteAccess(DirectoryInfo d)
+{
+    foreach (var file in d.GetFiles())
+    {
+        var hasAccess = true;
+        try
+        {
+            hasAccess = HasWriteAccess(file.GetAccessControl());
+        }
+        catch (UnauthorizedAccessException)
+        {
+            hasAccess = false;
+        }
+        if (!hasAccess)
+            yield return file;
+    };
+}
+
+static IEnumerable<DirectoryInfo> DirectoriesWithoutWriteAccess(DirectoryInfo d)
+{
+    foreach (var dir in d.GetDirectories())
+    {
+        var hasAccess = true;
+        try
+        {
+            hasAccess = HasWriteAccess(dir.GetAccessControl());
+        }
+        catch (UnauthorizedAccessException)
+        {
+            hasAccess = false;
+        }
+        if (!hasAccess)
+            yield return dir;
+    };
+}
+
+static bool HasWriteAccess(FileSystemSecurity security)
+{
+    try
+    {
+        return security
+            .GetAccessRules(true, true, typeof(NTAccount))
+            .OfType<FileSystemAccessRule>()
+            .Any(rule => rule.AccessControlType == AccessControlType.Allow &&
+                    (rule.FileSystemRights.HasFlag(FileSystemRights.Write) ||
+                     rule.FileSystemRights.HasFlag(FileSystemRights.Modify)));
+    }
+    catch (UnauthorizedAccessException)
+    {
+        return false;
+    }
+    catch (Exception ex)
+    {
+        throw;
+    }
+}
---
+++ ListFilePermissions/README.md
@@ -0,0 +1,21 @@
+﻿## One Drive support
+
+Sometimes the files that are constantly processing seem to be the result of the admin user not having modify rights to those files.
+This program creates a list of all files in the One Drive folder that the administrator does not have rights to.
+
+
+See this link:
+https://www.technewstoday.com/you-must-have-read-permissions-to-view-the-properties-of-this-object/
+for information on changing file permissions.
+
+# Take Ownership and Change Permissions Using CLI
+
+If you find it troublesome to execute the above method, you can use the Windows CLIs like Command Prompt to perform a similar process. It only requires two commands so it’s more convenient. The necessary steps are as follows:
+
+- Press Win + R to open Run.
+- Type cmd and press Ctrl + Shift + Enter to open the Elevated Command Prompt.
+- Enter the following commands while replacing “Folder With Error” with the problematic file’s full location path:
+```
+takeown /f “Folder With Error” /a /r /d y
+icacls “Folder With Error” /t /c /grant Administrators:F System:F
+```
